3e914df6e5f637e4f4053251f7bfed37c80980f9
rimjhimkumari23 <rimjhimkumari230707@gmail.com>
2026-02-06 22:23:25 +0530

    Add files via upload

---
+++ two line.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    printf("Hello \nWorld");
+    printf("\nGood \nMorning");
+
+    return 0;
+}
---
+++ update var.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+int a=10;
+    int b=50;
+    //update the value of a
+    a=20;
+    b=100;
+    printf("%d\n%d",a,b);
+    return 0;
+}
